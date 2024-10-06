0b4fc541b2fe55de555fadbe8c7c4add0522c4c8
Théo Santos <theo.dellacqua@gmail.com>
2024-10-05 22:22:40 -0300

    exercicio 1.1 even numbers com foreach implementado

--- exercicios_estrutura_repeticao.sql
+++ exercicios_estrutura_repeticao.sql
@@ -41,3 +41,17 @@ BEGIN
 END;
 $$
 
+--Exercicio 1.1 - Even Numbers com FOREACH
+DO
+$$
+DECLARE
+    lista_contador INT[] := ARRAY[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+    contador INT;
+BEGIN
+    FOREACH contador IN ARRAY lista_contador LOOP
+        CONTINUE WHEN contador % 2 <> 0;
+            RAISE NOTICE '%', contador;
+    END LOOP;
+END;
+$$
+
